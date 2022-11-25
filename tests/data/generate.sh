f7f45193709d920578be092b79388df3aa86c5e0
Roben Kleene <contact@robenkleene.com>
2022-11-25 17:32:30 +0000

    Generate patch file

--- tests/data/generate.sh
+++ tests/data/generate.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 grep --line-number --with-filename Markdown markdown-syntax.md > grep.txt
-diff markdown-syntax.md <(sed s/Markdown/Markup/g markdown-syntax.md)
+diff --unified markdown-syntax.md <(sed s/Markdown/Markup/g markdown-syntax.md) > markdown-markup.patch || true
