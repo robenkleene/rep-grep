5d62c6e4551d6141a4c0f338e6d287df00777fd9
Roben Kleene <contact@robenkleene.com>
2022-11-25 16:12:22 -0500

    Add test data (#1)
    * Update generate
    
    * Output grep file
    
    * Work on patch file
    
    * Generate patch file

--- tests/data/generate.sh
+++ tests/data/generate.sh
@@ -2,3 +2,5 @@
 
 set -euo pipefail
 
+grep --line-number --with-filename Markdown markdown-syntax.md > grep.txt
+diff --unified markdown-syntax.md <(sed s/Markdown/Markup/g markdown-syntax.md) > markdown-markup.patch || true
