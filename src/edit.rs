3a419bac11b66925a1a80202cda5a49b345986c0
Roben Kleene <contact@robenkleene.com>
2022-12-03 06:49:08 -0500

    Add edit (#4)
    * Add parser
    
    * Fix test compile errors
    
    * Add custom error types
    
    * Add file, number, and text tests
    
    * Add edit test
    
    * Add count to test data
    
    * Improve patch file paths
    
    * Rename parser to edit

---
+++ src/edit.rs
@@ -0,0 +1,121 @@
+use std::path::PathBuf;
+use regex::Regex;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+#[derive(Debug)]
+pub(crate) struct Edit {
+    file: PathBuf,
+    text: String,
+    number: u32
+}
+
+#[derive(Debug)]
+#[derive(thiserror::Error)]
+pub enum Error {
+  #[error("No file, number, and text matches")]
+  Match,
+}
+
+impl Edit {
+    fn new(file: PathBuf, text: String, number: u32) -> Edit {
+        Edit { file, text, number }
+    }
+
+    pub(crate) fn parse<R> (
+        reader: BufReader<R>
+    ) -> Result<Vec::<Edit>, std::io::Error>
+        where R: Read
+    {
+        let mut edits = Vec::<Edit>::new();
+        for line in reader.lines() {
+            let line = match line {
+                Ok(line) => line,
+                Err(_) => continue,
+            };
+            let line = match Self::edit_from_line(line) {
+                Ok(line) => line,
+                Err(_) => continue,
+            };
+            edits.push(line);
+        }
+        return Ok(edits);
+    }
+
+    fn edit_from_line(line: String) -> Result<Edit, Error> {
+        let re = Regex::new("^([^:]+):([[:digit:]]+):(.*)$").unwrap();
+        let caps = re.captures(&line);
+        let caps = match caps {
+            Some(caps) => caps,
+            None => return Err(Error::Match),
+        };
+        let file = match caps.get(1) {
+            Some(file) => PathBuf::from(file.as_str()),
+            None => return Err(Error::Match),
+        };
+        let number = match caps.get(2) {
+            Some(number) => number.as_str(),
+            None => return Err(Error::Match),
+        };
+        let number = match number.parse::<u32>() {
+            Ok(number) => number,
+            Err(_) => return Err(Error::Match),
+        };
+        let mut text = match caps.get(3) {
+            Some(text) => text.as_str().to_string(),
+            None => return Err(Error::Match),
+        };
+
+        // Check for the optional column and discard it if present
+        let text_re = Regex::new("^([[:digit:]]+):(.*)$").unwrap();
+        let text_caps = text_re.captures(&line);
+        if let Some(text_caps) = text_caps {
+            text = match text_caps.get(2) {
+                Some(text) => text.as_str().to_string(),
+                None => text,
+            };
+        }
+
+        return Ok(Edit::new(
+            file,
+            text,
+            number,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn edit_from_line_no_file() {
+        let result = Edit::edit_from_line("aaa".to_string());
+        assert!(matches!(result, Err(Error::Match)));
+    }
+
+    #[test]
+    fn edit_from_line_no_number() {
+        let result = Edit::edit_from_line("aaa.txt:bbb:ccc".to_string());
+        assert!(matches!(result, Err(Error::Match)));
+    }
+
+    #[test]
+    fn edit_from_line_no_text() {
+        let result = Edit::edit_from_line("aaa.txt:1".to_string());
+        assert!(matches!(result, Err(Error::Match)));
+    }
+
+    #[test]
+    fn edit_from_line() {
+        let result = Edit::edit_from_line("aaa.txt:1:text".to_string());
+        assert!(matches!(result, Ok(_)));
+        let edit = match result {
+            Ok(result) => result,
+            Err(_) => panic!("Unexpected error"),
+        };
+        assert_eq!(edit.file, PathBuf::from("aaa.txt"));
+        assert_eq!(edit.number, 1);
+        assert_eq!(edit.text, "text");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cli;
 mod error;
 mod input;
+mod edit;
 pub(crate) mod replacer;
 pub(crate) mod utils;
 
