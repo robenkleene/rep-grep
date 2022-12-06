ec2ad444d85dcdb4acf384654fc56559cd6031f5
Roben Kleene <contact@robenkleene.com>
2022-12-06 17:35:58 +0000

    Add patcher test

--- src/patcher.rs
+++ src/patcher.rs
@@ -1,25 +1,25 @@
 use crate::edit::Edit;
-use std::convert::TryInto;
 use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 pub(crate) struct Patcher {
-    edits: Vec::<Edit>
+    edits: Vec<Edit>,
 }
 
-#[derive(Debug)]
-#[derive(thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-  #[error("Invalid line number")]
-  LineNumber,
+    #[error("Invalid line number")]
+    LineNumber,
 }
 
 impl Patcher {
-    pub(crate) fn new(edits: Vec::<Edit>) -> Self {
+    pub(crate) fn new(edits: Vec<Edit>) -> Self {
         Self { edits }
     }
 
-    pub(crate) fn patch(&self, mut lines: Vec::<String>) -> Result<String, Error> {
+    pub(crate) fn patch(&self, mut lines: Vec<String>) -> Result<String, Error> {
         let len = lines.len();
         for edit in &self.edits {
             if edit.number > len.try_into().unwrap() {
@@ -28,6 +28,31 @@ impl Patcher {
             let index = usize::try_from(edit.number).unwrap();
             lines[index] = edit.text.clone();
         }
-        return Ok(lines.join(""));
+        return Ok(lines.join("\n"));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn patch() {
+        let patcher = Patcher::new(vec![
+            Edit {
+                file: PathBuf::from("a"),
+                number: 1,
+                text: "foo".to_string(),
+            },
+            Edit {
+                file: PathBuf::from("a"),
+                number: 2,
+                text: "bar".to_string(),
+            },
+        ]);
+        let lines = vec!["a".to_string(), "b".to_string(), "c".to_string()];
+        let result = patcher.patch(lines);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "a\nfoo\nbar");
     }
 }
