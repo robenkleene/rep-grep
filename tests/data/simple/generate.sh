13954a43ce9cef0a5838054cd5f13a0cf29cffe3
Roben Kleene <contact@robenkleene.com>
2023-02-02 05:44:20 -0500

    WIP Add generated test assets

--- tests/data/simple/generate.sh
+++ tests/data/simple/generate.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-grep --line-number --with-filename bomp original.md > grep.txt
-diff --unified original.md \
-  <(sed s/bomp/ram/g original.md) > patch.patch || true
+grep --line-number --with-filename bomp original.txt > grep.txt
+diff --unified original.txt \
+  <(sed s/bomp/ram/g original.txt) > patch.patch || true
 sed -i '' '1s/.*/--- a\/original.md/' patch.patch
 sed -i '' '2s/.*/+++ b\/original.md/' patch.patch
 wc -l < grep.txt | xargs > grep-count.txt
