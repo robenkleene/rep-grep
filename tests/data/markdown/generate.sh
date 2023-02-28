fc06b04f19d16b551b48cfef44be79c084446524
Roben Kleene <contact@robenkleene.com>
2023-02-28 05:45:43 -0500

    Fix filename

--- tests/data/markdown/generate.sh
+++ tests/data/markdown/generate.sh
@@ -9,4 +9,4 @@ diff --unified markdown-syntax.md \
   <(sed s/Markdown/Markup/g markdown-syntax.md) > markdown-markup.patch || true
 sed -i '' '1s/.*/--- a\/markdown-syntax.md/' markdown-markup.patch
 sed -i '' '2s/.*/+++ b\/markdown-syntax.md/' markdown-markup.patch
-wc -l < markdown-grep.txt | xargs > grep-count.txt
+wc -l < markdown-to-markup-grep.txt | xargs > grep-count.txt
