cbfbdde89e750ede7eba7aac2c745536da220a56
Roben Kleene <contact@robenkleene.com>
2023-03-02 05:30:43 -0500

    Remove whitespace lines

--- tests/data/markdown/generate.sh
+++ tests/data/markdown/generate.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")" || exit 1
 grep --line-number --with-filename Markdown markdown-syntax.md > markdown-to-markup-grep.txt
 diff --unified markdown-syntax.md \
   <(sed s/Markdown/Markup/g markdown-syntax.md) > markdown-markup.patch || true
+sed -i '' 's/^ $//' markdown-markup.patch
 sed -i '' '1s/.*/--- a\/markdown-syntax.md/' markdown-markup.patch
 sed -i '' '2s/.*/+++ b\/markdown-syntax.md/' markdown-markup.patch
 sed -i '' 's/Markdown/Markup/g' markdown-to-markup-grep.txt
