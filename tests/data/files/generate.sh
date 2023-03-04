44facf466aa969aff39c81bece1d6822881f55d8
Roben Kleene <contact@robenkleene.com>
2023-03-04 05:39:42 -0500

    Update multi-file example

--- tests/data/files/generate.sh
+++ tests/data/files/generate.sh
@@ -6,11 +6,11 @@ cd "$(dirname "$0")" || exit 1
 
 grep --line-number --with-filename change 1.txt 2.txt 3.txt > grep.txt
 diff --unified 1.txt \
-  <(sed s/change/altered/g 1.txt) > patch.patch || true
+  <(sed s/changes/altered/g 1.txt) > patch.patch || true
 diff --unified 2.txt \
-  <(sed s/change/altered/g 1.txt) > patch.patch || true
+  <(sed s/changes/altered/g 2.txt) >> patch.patch || true
 diff --unified 3.txt \
-  <(sed s/change/altered/g 1.txt) > patch.patch || true
+  <(sed s/changes/altered/g 3.txt) >> patch.patch || true
 wc -l < 1.txt | xargs > 1-count.txt
 wc -l < 2.txt | xargs > 2-count.txt
 wc -l < 3.txt | xargs > 3-count.txt
