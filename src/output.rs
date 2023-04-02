50c05d556863b53e650fe10379ee13f460908540
Roben Kleene <contact@robenkleene.com>
2023-04-02 05:40:55 -0400

    WIP Migrate over more delta pager functions

--- src/output.rs
+++ src/output.rs
@@ -20,7 +20,6 @@ impl Output {
         pager: Option<String>,
         quit_if_one_screen: bool,
     ) -> Result<()> {
-
         let pager = pager.unwrap_or_else(|| String::from("less"));
         let pagerflags = match shell_words::split(&pager) {
             Ok(pagerflags) => pagerflags,
@@ -32,15 +31,14 @@ impl Output {
                 let pager_path = PathBuf::from(pager_name);
 
                 let is_less = pager_path.file_stem() == Some(&OsString::from("less"));
-
                 let process = if is_less {
-                    _make_process_from_less_path(
+                    make_process_from_less_path(
                         pager_path,
                         args,
                         quit_if_one_screen,
                     )
                 } else {
-                    _make_process_from_pager_path(pager_path, args)
+                    make_process_from_pager_path(pager_path, args)
                 };
                 if let Some(mut process) = process {
                     process
@@ -55,46 +53,64 @@ impl Output {
             None => OutputType::stdout(),
         })
     }
-}
 
-fn _make_process_from_less_path(
-    less_path: PathBuf,
-    args: &[String],
-    quit_if_one_screen: bool,
-) -> Option<Command> {
-    if let Ok(less_path) = grep_cli::resolve_binary(less_path) {
-        let mut p = Command::new(&less_path);
-        if args.is_empty() || replace_arguments_to_less {
-            p.args(vec!["--RAW-CONTROL-CHARS"]);
+    fn make_process_from_less_path(
+        less_path: PathBuf,
+        args: &[String],
+        quit_if_one_screen: bool,
+    ) -> Option<Command> {
+        if let Ok(less_path) = grep_cli::resolve_binary(less_path) {
+            let mut p = Command::new(&less_path);
+            if args.is_empty() || replace_arguments_to_less {
+                p.args(vec!["--RAW-CONTROL-CHARS"]);
 
-            // Passing '--no-init' fixes a bug with '--quit-if-one-screen' in older
-            // versions of 'less'. Unfortunately, it also breaks mouse-wheel support.
-            //
-            // See: http://www.greenwoodsoftware.com/less/news.530.html
-            //
-            // For newer versions (530 or 558 on Windows), we omit '--no-init' as it
-            // is not needed anymore.
-            match retrieve_less_version() {
-                None => {
-                    p.arg("--no-init");
+                // Passing '--no-init' fixes a bug with '--quit-if-one-screen' in older
+                // versions of 'less'. Unfortunately, it also breaks mouse-wheel support.
+                //
+                // See: http://www.greenwoodsoftware.com/less/news.530.html
+                //
+                // For newer versions (530 or 558 on Windows), we omit '--no-init' as it
+                // is not needed anymore.
+                match retrieve_less_version() {
+                    None => {
+                        p.arg("--no-init");
+                    }
+                    Some(version) if (version < 530 || (cfg!(windows) && version < 558)) => {
+                        p.arg("--no-init");
+                    }
+                    _ => {}
                 }
-                Some(version) if (version < 530 || (cfg!(windows) && version < 558)) => {
-                    p.arg("--no-init");
-                }
-                _ => {}
-            }
 
-            if quit_if_one_screen {
-                p.arg("--quit-if-one-screen");
+                if quit_if_one_screen {
+                    p.arg("--quit-if-one-screen");
+                }
+            } else {
+                p.args(args);
             }
+            p.env("LESSCHARSET", "UTF-8");
+            p.env("LESSANSIENDCHARS", "mK");
+            Some(p)
         } else {
-            p.args(args);
+            None
         }
-        p.env("LESSCHARSET", "UTF-8");
-        p.env("LESSANSIENDCHARS", "mK");
-        Some(p)
-    } else {
-        None
+    }
+
+    fn make_process_from_pager_path(pager_path: PathBuf, args: &[String]) -> Option<Command> {
+        if pager_path.file_stem() == Some(&OsString::from("delta")) {
+            fatal(
+                "\
+    It looks like you have set delta as the value of $PAGER. \
+    This would result in a non-terminating recursion. \
+    delta is not an appropriate value for $PAGER \
+    (but it is an appropriate value for $GIT_PAGER).",
+            );
+        }
+        if let Ok(pager_path) = grep_cli::resolve_binary(pager_path) {
+            let mut p = Command::new(&pager_path);
+            p.args(args);
+            Some(p)
+        } else {
+            None
+        }
     }
 }
-
