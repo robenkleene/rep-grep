86977cd2d158b87c24aea7c568202a1a27cb09a0
Roben Kleene <contact@robenkleene.com>
2023-07-03 02:00:27 +0000

    Work on fixing generate script

--- tests/data/delete/generate.sh
+++ tests/data/delete/generate.sh
@@ -11,3 +11,9 @@ diff --unified 2.txt \
   <(sed /delete/d 2.txt) >> patch.patch || true
 diff --unified 3.txt \
   <(sed /delete/d 3.txt) >> patch.patch || true
+sed -i '' '1s/.*/--- a\/1.txt/' patch.patch
+sed -i '' '2s/.*/+++ b\/1.txt/' patch.patch
+sed -i '' '6s/.*/--- a\/2.txt/' patch.patch
+sed -i '' '7s/.*/+++ b\/2.txt/' patch.patch
+sed -i '' '13s/.*/--- a\/3.txt/' patch.patch
+sed -i '' '14s/.*/+++ b\/3.txt/' patch.patch
