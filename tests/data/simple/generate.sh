4a0308575c5315133b9edea6d1611b96edd842e2
Roben Kleene <contact@robenkleene.com>
2023-10-07 05:27:59 -0400

    Add vimgrep example

--- tests/data/simple/generate.sh
+++ tests/data/simple/generate.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 grep --line-number --with-filename bomp original.txt > grep.txt
+rg --vimgrep bomp original.txt > vimgrep.txt
+
 diff --unified original.txt \
   <(sed s/bomp/ram/g original.txt) > patch.patch || true
 sed -i '' 's/bomp/ram/g' grep.txt
