c285a31a0f34385a327351d98fb626e321031e08
Roben Kleene <contact@robenkleene.com>
2023-10-11 05:19:56 -0400

    Fix test output

--- tests/data/simple/generate.sh
+++ tests/data/simple/generate.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 grep --line-number --with-filename bomp original.txt > grep.txt
-rg --vimgrep bomp original.txt > vimgrep.txt
 
 diff --unified original.txt \
   <(sed s/bomp/ram/g original.txt) > patch.patch || true
@@ -15,3 +14,6 @@ sed -i '' '2s/.*/+++ b\/original.txt/' patch.patch
 sed -i '' '3s/-1,2 +1,2/-1 +1/' patch.patch
 sed -i '' '6d' patch.patch
 wc -l < grep.txt | xargs > grep-count.txt
+
+rg --vimgrep bomp original.txt > vimgrep.txt
+sed -i '' 's/bomp/ram/g' vimgrep.txt
